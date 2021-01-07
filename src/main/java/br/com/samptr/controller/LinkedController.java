8979602693a1b65e2c9644d73cf92ecd64888bf6
marcos <marcos.rib.sousa@gmail.com>
2021-01-07 17:53:59 -0300

    Ajuste page

--- src/main/java/br/com/samptr/controller/LinkedController.java
+++ src/main/java/br/com/samptr/controller/LinkedController.java
@@ -4,13 +4,18 @@ package br.com.samptr.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.samptr.dtos.LinkedDTO;
 import br.com.samptr.models.LinkedEntity;
+import br.com.samptr.repositories.LinkedRepository;
 import br.com.samptr.services.LinkedService;
 
 @Controller
@@ -20,19 +25,24 @@ public class LinkedController {
 	
 	@Autowired
 	private LinkedService service;
+	@Autowired
+	private LinkedRepository repository;
 	
 	@GetMapping(value = "/create-link")
-	public String getLink() {
+	public String getLink( ModelMap model) {
+		
        return "redirect:/";
 	}
 	
 	@PostMapping(value = "/create-link")
-	public String create(@Validated LinkedDTO dto, BindingResult result, Model model) {
+	public ModelAndView create(@Validated LinkedDTO dto, BindingResult result, ModelMap model, RedirectAttributes redirectAttrs) {
 		LinkedEntity linkendEntity  = service.save(convertDTOToEntity(dto));
 		LinkedDTO dtoConverted = convertEntityToDTO(linkendEntity);
-		dtoConverted.setUrlDownload(URL + linkendEntity.getHashUri() );
-		model.addAttribute("linked", dtoConverted);
-		return "/index";
+		 dtoConverted.setUrlDownload(URL + linkendEntity.getHashUri() );
+	
+		  ModelAndView modelAndView = new ModelAndView("index");
+		    modelAndView.addObject("message", "Baeldung");
+		return modelAndView;
 	}
 
 	private LinkedEntity convertDTOToEntity(LinkedDTO dto) {
