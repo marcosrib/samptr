5014053d863f179c45083f1ab9cafc614b98e774
marcos <marcos.rib.sousa@gmail.com>
2021-01-07 18:12:40 -0300

    Ajuste page

--- src/main/java/br/com/samptr/controller/LinkedController.java
+++ src/main/java/br/com/samptr/controller/LinkedController.java
@@ -1,14 +1,11 @@
 package br.com.samptr.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -22,40 +19,32 @@ import br.com.samptr.services.LinkedService;
 public class LinkedController {
 
 	private static final String URL = "http://localhost:8080/download/";
-	
+
 	@Autowired
 	private LinkedService service;
-	@Autowired
-	private LinkedRepository repository;
-	
-	//@GetMapping(value = "/create-link")
-	public String getLink( ModelMap model) {
-		
-       return "redirect:/";
+
+	@GetMapping(value = "/create-link")
+	public String getLink() {
+		return "redirect:/";
 	}
-	
+
 	@PostMapping(value = "/create-link")
-	public ModelAndView create(@Validated LinkedDTO dto, BindingResult result, ModelMap model, RedirectAttributes redirectAttrs) {
-		LinkedEntity linkendEntity  = service.save(convertDTOToEntity(dto));
+	public ModelAndView create(@Validated LinkedDTO dto, BindingResult result, ModelMap model,
+			RedirectAttributes redirectAttrs) {
+		LinkedEntity linkendEntity = service.save(convertDTOToEntity(dto));
 		LinkedDTO dtoConverted = convertEntityToDTO(linkendEntity);
-		 dtoConverted.setUrlDownload(URL + linkendEntity.getHashUri() );
-	
-		  ModelAndView modelAndView = new ModelAndView("index");
-		    modelAndView.addObject("message", "Baeldung");
+		dtoConverted.setUrlDownload(URL + linkendEntity.getHashUri());
+		ModelAndView modelAndView = new ModelAndView("index");
+		modelAndView.addObject("link", dtoConverted.getUrlDownload());
 		return modelAndView;
 	}
 
 	private LinkedEntity convertDTOToEntity(LinkedDTO dto) {
-		return LinkedEntity.builder()
-				.uri(dto.getUri())
-				.build();
+		return LinkedEntity.builder().uri(dto.getUri()).build();
 	}
-	
+
 	private LinkedDTO convertEntityToDTO(LinkedEntity entity) {
-		return LinkedDTO.builder()
-				.id(entity.getId())
-				.build();
+		return LinkedDTO.builder().id(entity.getId()).build();
 	}
-	
-	
+
 }
